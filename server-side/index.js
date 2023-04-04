17eb862be30277284713c8800a26ee469d5ebe7e
ShafiArnob <shafiarnob@gmail.com>
2023-04-04 09:37:18 +0600

    api single product

--- server-side/index.js
+++ server-side/index.js
@@ -23,6 +23,13 @@ async function run(){
       const products = await cursor.toArray()
       res.send(products)
     })
+
+    app.get('/api/products/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id:ObjectId(id)}
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    })
   }
   finally{
 
